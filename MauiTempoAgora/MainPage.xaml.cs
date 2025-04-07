2c606ce219fd097f372ce349c7691e37f189bb8a
231009 <231009@CPD03-M07>
2025-04-07 11:28:37 -0300

    Feita a tela principal, falta o back. Programa é do capeta.

--- MauiTempoAgora/MainPage.xaml.cs
+++ MauiTempoAgora/MainPage.xaml.cs
@@ -1,25 +1,61 @@
-﻿namespace MauiTempoAgora
+﻿using MauiTempoAgora.Models;
+using MauiTempoAgora.Services;
+using System;
+using System.Diagnostics;
+
+namespace MauiTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void ButtonLocalizacao_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if(!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if(t != null)
+                    {
+                        string dados_previsao = "";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temperatura Máxima: {t.tempmax} \n" +
+                                         $"Temperatura Mínima: {t.tempmin} \n";
+
+                        lbl_res.Text = dados_previsao;
+
+                        string mapa = $"https://embed.windy.com/embed.html?" +
+                                      $"type=map&location=coordinates&metricRain=mm&metricTemp=°C" +
+                                      $"&metricWind=km/h&zoom=5&overlay=wind&product=ecmwf&level=surface" +
+                                      $"&lat={t.lat.ToString().Replace(",", ".")}&lon={t.lon.ToString().Replace(",", ".")}";
+                    
+                        wv_mapa.Source = mapa;
+
+                        Debug.WriteLine(mapa);
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dados de previsão.";
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
-
 }
