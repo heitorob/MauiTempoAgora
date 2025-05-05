78de7c0eb492767ed580dc86a6214ad6f4a15078
231009 <231009@CPD03-M07>
2025-05-05 10:52:45 -0300

    Início da criação da consulta.

---
+++ MauiTempoAgora/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiTempoAgora.Models;
+using SQLite;
+
+namespace MauiTempoAgora.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection conectar;
+
+        public SQLiteDatabaseHelper(string rota)
+        {
+            conectar = new SQLiteAsyncConnection(rota);
+            conectar.CreateTableAsync<Previsao>().Wait();
+        }
+    }
+}
