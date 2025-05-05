7a7f8ad81d5bead10bad03c4e4b9d6f38058fc07
231009 <231009@CPD03-M07>
2025-05-05 11:36:16 -0300

    Criação do Helper. Talvez eu sei o que estou fazendo.

--- MauiTempoAgora/Helpers/SQLiteDatabaseHelper.cs
+++ MauiTempoAgora/Helpers/SQLiteDatabaseHelper.cs
@@ -17,5 +17,32 @@ namespace MauiTempoAgora.Helpers
             conectar = new SQLiteAsyncConnection(rota);
             conectar.CreateTableAsync<Previsao>().Wait();
         }
+
+        public Task<int> Insert(Previsao p)
+        {
+            return conectar.InsertAsync(p);
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return conectar.Table<Previsao>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Previsao>> GetAll()
+        {
+            return conectar.Table<Previsao>().ToListAsync();
+        }
+
+        public Task<List<Previsao>> SearchByDate(DateTime q)
+        {
+            string sql = "SELECT * FROM Previsao WHERE data LIKE ?";
+            return conectar.QueryAsync<Previsao>(sql, q);
+        }
+
+        public Task<List<Previsao>> SearchByLocation(string q)
+        {
+            string sql = "SELECT * FROM Previsao WHERE cidade LIKE ?";
+            return conectar.QueryAsync<Previsao>(sql, "%",  q, "%");
+        }
     }
 }
--- MauiTempoAgora/Models/Previsao.cs
+++ MauiTempoAgora/Models/Previsao.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace MauiTempoAgora.Models
 {
     public class Previsao
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public DateTime data { get => DateTime.Now; }
+        public string cidade { get; set; }
+        public string temperatura { get; set; }
+        public string descricao { get; set; }
     }
 }
